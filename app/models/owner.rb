3ec54ccdcb487f3bec53893283c8aa6f7404611a
shahsarefin <shahsarefin@gmail.com>
2024-03-10 12:10:30 -0500

    Owner model updated

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -3,6 +3,8 @@ class Owner < ApplicationRecord
     has_many :breeds, through: :adoptions
   
     validates :name, presence: true
-    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :phone_number, presence: true
+    validates :address, presence: true
   end
   
