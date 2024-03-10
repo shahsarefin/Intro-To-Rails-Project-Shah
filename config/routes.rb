279a9cd7d62421b61001eabe91ea767fa8e586a9
shahsarefin <shahsarefin@gmail.com>
2024-03-10 12:05:38 -0500

    routes updated for breeds/dogs

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get 'owners/index'
+  # route for the breeds index action
+  root 'breeds#index'
+  get '/breeds', to: 'breeds#index', as: :breeds
+  #get '/owners', to: 'owners#index', as: :owners
 end
