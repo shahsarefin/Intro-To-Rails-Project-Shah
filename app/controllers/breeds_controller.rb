37cea89f2c47d8cd66f0db85015ff51344b49ea9
shahsarefin <shahsarefin@gmail.com>
2024-03-10 12:02:16 -0500

    Data Source 1: Fetched Data from Dog API by breeds_controller

---
+++ app/controllers/breeds_controller.rb
@@ -0,0 +1,16 @@
+require 'httparty'
+
+class BreedsController < ApplicationController
+    def index
+      response = HTTParty.get('https://api.thedogapi.com/v1/breeds')
+  
+      if response.success?
+        all_breeds = JSON.parse(response.body)
+        @breeds = Kaminari.paginate_array(all_breeds).page(params[:page]).per(15)
+      else
+        @breeds = Kaminari.paginate_array([]).page(params[:page]).per(15)
+        flash[:error] = "There was a problem fetching the breed data."
+      end
+    end
+  end
+  
