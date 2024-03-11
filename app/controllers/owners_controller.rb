3742dd24cc7b3e9152046ddd83bc77a2c55ae465
shahsarefin <shahsarefin@gmail.com>
2024-03-11 09:12:12 -0500

    Owner Controller updated for owner show page

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -2,4 +2,8 @@ class OwnersController < ApplicationController
   def index
     @owners = Owner.all.page(params[:page]).per(15)
   end
+
+  def show
+    @owner = Owner.find(params[:id])
+  end
 end
