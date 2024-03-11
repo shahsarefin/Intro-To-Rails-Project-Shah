0e78f25ddce375dcf70f981fbdedbe9592f41137
shahsarefin <shahsarefin@gmail.com>
2024-03-11 08:33:31 -0500

    Member page: Details Page Added for Dogs

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -14,4 +14,37 @@ class BreedsController < ApplicationController
       end
     end
   end
+
+  class BreedsController < ApplicationController
+    def index
+      response = HTTParty.get('https://api.thedogapi.com/v1/breeds')
+    
+      if response.success?
+        all_breeds = JSON.parse(response.body)
+        @breeds = Kaminari.paginate_array(all_breeds).page(params[:page]).per(15)
+      else
+        @breeds = Kaminari.paginate_array([]).page(params[:page]).per(15)
+        flash[:error] = "There was a problem fetching the breed data."
+      end
+    end
+  
+    def show
+      response = HTTParty.get('https://api.thedogapi.com/v1/breeds')
+      if response.success?
+        all_breeds = JSON.parse(response.body)
+        @breed = all_breeds.find { |breed| breed['id'].to_s == params[:id] }
+      end
+    
+      unless @breed
+        flash[:error] = "There was a problem fetching the breed data."
+        redirect_to breeds_path and return
+      end
+    end
+  end
+  
+  
+  
+  
+  
+  
   
