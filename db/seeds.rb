1b83a5b31ee8486a3433c584b94a30a43f3c38c4
shahsarefin <shahsarefin@gmail.com>
2024-03-12 19:51:58 -0500

    Implemented model associations and updated seeding process with external API and CSV data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,36 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'httparty'
+require 'csv'
+
+response = HTTParty.get('https://api.thedogapi.com/v1/breeds')
+if response.success?
+  breeds = JSON.parse(response.body)
+  breeds.each do |breed|
+    Breed.find_or_create_by(name: breed['name'], temperament: breed['temperament'] || "Unknown")
+  end
+else
+  puts "Failed to fetch breeds from The Dog API"
+end
+
+
+csv_text = File.read(Rails.root.join('db', 'owners.csv'))
+csv = CSV.parse(csv_text, headers: true, encoding: 'ISO-8859-1')
+csv.each do |row|
+  Owner.find_or_create_by(name: row['name'], email: row['email'], phone_number: row['phone_number'], address: row['address'])
+end
+
+
+
+# 100 Adoptions
+100.times do
+  Adoption.create!(
+    breed_id: Breed.all.sample.id,
+    owner_id: Owner.all.sample.id,
+    adoption_date: Faker::Date.between(from: 2.years.ago, to: Date.today)
+  )
+end
+
+  
