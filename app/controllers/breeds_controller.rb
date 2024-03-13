4a4843b0065beb0d5532c29bcaf328ec34182acf
shahsarefin <shahsarefin@gmail.com>
2024-03-12 20:31:08 -0500

    Breeds Controller and show page updated to show association with owners by adoption

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -1,20 +1,5 @@
 require 'httparty'
 
-class BreedsController < ApplicationController
-    def index
-      response = HTTParty.get('https://api.thedogapi.com/v1/breeds')
-  
-      if response.success?
-        all_breeds = JSON.parse(response.body)
-        # paginated the breeds with kaminari
-        @breeds = Kaminari.paginate_array(all_breeds).page(params[:page]).per(15)
-      else
-        @breeds = Kaminari.paginate_array([]).page(params[:page]).per(15)
-        flash[:error] = "There was a problem fetching the breed data."
-      end
-    end
-  end
-
   class BreedsController < ApplicationController
     def index
       response = HTTParty.get('https://api.thedogapi.com/v1/breeds')
@@ -39,6 +24,10 @@ class BreedsController < ApplicationController
         flash[:error] = "There was a problem fetching the breed data."
         redirect_to breeds_path and return
       end
+
+    #Assosiation with owners
+    local_breed = Breed.find_by(name: @breed['name']) 
+    @owners = local_breed ? local_breed.owners.distinct : []
     end
   end
   
