21a2abf9bcb6fa1ab841cfa37987345f0834da7d
shahsarefin <shahsarefin@gmail.com>
2024-03-12 22:58:51 -0500

    Multimodel data: from owners to breeds through adoption & breeds_controller updated

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -14,23 +14,20 @@ require 'httparty'
   
       @breeds = Kaminari.paginate_array(@breeds).page(params[:page]).per(15)
     end
-  
-    def show
-      response = HTTParty.get('https://api.thedogapi.com/v1/breeds')
-      if response.success?
-        all_breeds = JSON.parse(response.body)
-        @breed = all_breeds.find { |breed| breed['id'].to_s == params[:id] }
-      end
-    
-      unless @breed
-        flash[:error] = "There was a problem fetching the breed data."
-        redirect_to breeds_path and return
-      end
 
-    #Assosiation with owners
-    local_breed = Breed.find_by(name: @breed['name']) 
-    @owners = local_breed ? local_breed.owners.distinct : []
+    def show
+      response = HTTParty.get("https://api.thedogapi.com/v1/breeds/#{params[:id]}")
+      @breed = response.success? ? response.parsed_response : nil
+    
+      if @breed
+        local_breed = Breed.find_by(name: @breed['name'])
+        @adoptions = local_breed ? local_breed.adoptions.includes(:owner) : []
+      else
+        flash[:error] = "There was a problem fetching the breed data."
+        redirect_to breeds_path
+      end
     end
+    
   end
   
   
