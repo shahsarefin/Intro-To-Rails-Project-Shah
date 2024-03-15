ddfc2bb13ce9743bab55259f5fe878d0cd0bf05a
shahsarefin <shahsarefin@gmail.com>
2024-03-14 19:10:59 -0500

    Hierarchical Search Added

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -11,8 +11,18 @@ require 'httparty'
       else
         @breeds = all_breeds
       end
+
+       # Hierarchical search
+    if params[:bred_for].present?
+      all_breeds.select! { |breed| breed['bred_for']&.downcase&.include?(params[:bred_for].downcase) }
+    end
+
+    if params[:breed_group].present?
+      all_breeds.select! { |breed| breed['breed_group'] == params[:breed_group] }
+    end
   
       @breeds = Kaminari.paginate_array(@breeds).page(params[:page]).per(10)
+
     end
 
     def show
@@ -27,6 +37,20 @@ require 'httparty'
         redirect_to breeds_path
       end
     end
+
+    # app/controllers/breeds_controller.rb
+class BreedsController < ApplicationController
+  def index
+    @breeds = Breed.all
+    @breeds = @breeds.where(breed_group: params[:breed_group]) if params[:breed_group].present?
+    @breeds = @breeds.where("temperament ILIKE ?", "%#{params[:temperament]}%") if params[:temperament].present?
+    @breeds = @breeds.where("weight >= ?", params[:min_weight]) if params[:min_weight].present?
+    @breeds = @breeds.where("weight <= ?", params[:max_weight]) if params[:max_weight].present?
+    @breeds = @breeds.where("height >= ?", params[:min_height]) if params[:min_height].present?
+    @breeds = @breeds.where("height <= ?", params[:max_height]) if params[:max_height].present?
+  end
+end
+
     
   end
   
